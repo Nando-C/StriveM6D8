74310d86c9180f5ad60cc5efacfb006d47799aa6
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-07-14 16:01:02 +0100

    adds skeleton for routers

---
+++ src/services/blog/index.js
@@ -0,0 +1,51 @@
+import { Router } from "express";
+import { BlogPost } from "../../db/index.js";
+import sequelize from "sequelize";
+
+const router = Router()
+
+router.route('/')
+    .get( async (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+    })
+    .post( async (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+    })
+
+router.route('/:postId')
+    .get( async (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+    })
+    .put( async (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+    })
+    .delete( async (req, res, next) => {
+        try {
+            
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+    })
+
+    export default router
